844a74000fee1273862ef0faa9004e9f5751626d
Maxime Dupont <m-dupont01@outlook.fr>
2022-12-10 16:53:03 +0100

    Finish symmetry work for compute forces

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,27 @@ int main(int argc, char *argv[])
     int name_length;
     MPI_Get_processor_name(hostname, &name_length);
 
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
 
     // number of bodies for each process
     // compute nb_body to work with for each process
     int nb_body = floor(NB_BODY_TOTAL / world_size);
+
+    int nb_body_max;
 
     int nb_body_left = NB_BODY_TOTAL - (nb_body * world_size);
     if(current_rank < nb_body_left)
     {
         nb_body++;
     }
+
+    if(current_rank == HOST_RANK)
+    {
+        nb_body_max = nb_body;
+    }
+    MPI_Bcast(&nb_body_max, 1, MPI_INT, HOST_RANK, MPI_COMM_WORLD);
+
 
 
     // init bodies
@@ -42,8 +53,8 @@ int main(int argc, char *argv[])
     memset(local_forces, 0, SENDED_DATA_SIZE * nb_body * sizeof(double));
 
     double *tmp_forces = nullptr;
-    tmp_forces = (double *)malloc((nb_body+1) * SENDED_DATA_SIZE * sizeof(double));
-    memset(tmp_forces, 0, SENDED_DATA_SIZE * (nb_body+1) * sizeof(double));
+    tmp_forces = (double *)malloc(nb_body_max * SENDED_DATA_SIZE * sizeof(double));
+    memset(tmp_forces, 0, SENDED_DATA_SIZE * nb_body_max * sizeof(double));
 
 
     double *masses = nullptr;
@@ -66,7 +77,7 @@ int main(int argc, char *argv[])
     local_positions = (double *)malloc(nb_body * SENDED_DATA_SIZE * sizeof(double));
 
     double *tmp_positions = nullptr;
-    tmp_positions = (double *)malloc((nb_body+1) * SENDED_DATA_SIZE * sizeof(double));
+    tmp_positions = (double *)malloc(nb_body_max * SENDED_DATA_SIZE * sizeof(double));
 
     for(int i = 0; i < nb_body * SENDED_DATA_SIZE; i+=SENDED_DATA_SIZE)
     {
@@ -85,7 +96,7 @@ int main(int argc, char *argv[])
     local_ids = (int *)malloc(nb_body * sizeof(int));
     
     int *tmp_ids = nullptr;
-    tmp_ids = (int *)malloc((nb_body+1) * sizeof(int));
+    tmp_ids = (int *)malloc(nb_body_max * sizeof(int));
     
     for(int i=0; i<nb_body; ++i){
         local_ids[i] = current_rank + (i * world_size);
@@ -113,7 +124,7 @@ int main(int argc, char *argv[])
                 double current_body_mass = masses[current_body_id];
 
 
-                for(int l = 0; l < nb_body + 1; ++l)
+                for(int l = 0; l < nb_body_max; ++l)
                 {
                     // get id of other body
                     int other_body_id = tmp_ids[l];
@@ -135,8 +146,8 @@ int main(int argc, char *argv[])
                     // update local forces and tmp forces
                     local_forces[k * SENDED_DATA_SIZE + FORCE_X_INDEX] += forces[0];
                     local_forces[k * SENDED_DATA_SIZE + FORCE_Y_INDEX] += forces[1];
-                    tmp_forces[l * SENDED_DATA_SIZE + FORCE_X_INDEX] += forces[0];
-                    tmp_forces[l * SENDED_DATA_SIZE + FORCE_Y_INDEX] += forces[1];
+                    tmp_forces[l * SENDED_DATA_SIZE + FORCE_X_INDEX] -= forces[0];
+                    tmp_forces[l * SENDED_DATA_SIZE + FORCE_Y_INDEX] -= forces[1];
 
                 }
 
@@ -144,9 +155,50 @@ int main(int argc, char *argv[])
 
 
 
-            // communication => sendrecve_replace
+            /* 2 - communication */
+
+            // send tmp_ids, tmp_pos and tmp_forces to next process
+            int dest_proc = (current_rank - 1 + world_size) % world_size;
+            int src_proc = (current_rank + 1) % world_size;
+            //std::cout << "src_proc : " << src_proc << ", current_rank : " << current_rank << ", dest_proc : " << dest_proc << std::endl;
+
+    
+            MPI_Sendrecv_replace(tmp_ids, nb_body_max, MPI_INT, dest_proc, 0, src_proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Sendrecv_replace(tmp_positions, nb_body_max * SENDED_DATA_SIZE, MPI_DOUBLE, dest_proc, 0, src_proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Sendrecv_replace(tmp_forces, nb_body_max * SENDED_DATA_SIZE, MPI_DOUBLE, dest_proc, 0, src_proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
+
+
         // last compute forces => local_forces + tmp_forces
+        for(int j=0; j<nb_body; ++j)
+        {
+            local_forces[j * SENDED_DATA_SIZE + FORCE_X_INDEX] += tmp_forces[j * SENDED_DATA_SIZE + FORCE_X_INDEX];
+            local_forces[j * SENDED_DATA_SIZE + FORCE_Y_INDEX] += tmp_forces[j * SENDED_DATA_SIZE + FORCE_Y_INDEX];
+
+            double mass_current = masses[local_ids[j]];
+            local_forces[j * SENDED_DATA_SIZE + FORCE_X_INDEX] *= -GRAVITATIONAL_CONSTANT * mass_current;
+            local_forces[j * SENDED_DATA_SIZE + FORCE_X_INDEX] *= -GRAVITATIONAL_CONSTANT * mass_current;
+
+            /* compute the new position and velocity of the body */
+            double acceleration[2] = {(local_forces[0] * DELTA_T) / mass_current, (local_forces[1] * DELTA_T) / mass_current};
+
+            double velocity[2] = {velocities[j * SENDED_DATA_SIZE + VELOCITY_X_INDEX] + acceleration[0],
+                                  velocities[j * SENDED_DATA_SIZE + VELOCITY_Y_INDEX] + acceleration[1] * DELTA_T};
+
+            double position[2] = {local_positions[j * SENDED_DATA_SIZE + POSITION_X_INDEX] + velocity[0] * DELTA_T,
+                                  local_positions[j * SENDED_DATA_SIZE + POSITION_Y_INDEX] + velocity[1] * DELTA_T};
+
+            /* update the body */
+            velocities[j * SENDED_DATA_SIZE + VELOCITY_X_INDEX] = velocity[0];
+            velocities[j * SENDED_DATA_SIZE + VELOCITY_Y_INDEX] = velocity[1];
+            local_positions[j * SENDED_DATA_SIZE + POSITION_X_INDEX] = position[0];
+            local_positions[j * SENDED_DATA_SIZE + POSITION_Y_INDEX] = position[1];
+        }
+
+
+        // reset forces
+        memset(local_forces, 0, SENDED_DATA_SIZE * nb_body * sizeof(double));
+        memset(tmp_forces, 0, SENDED_DATA_SIZE * nb_body_max * sizeof(double));
 
     }
 
